5dd18813ceb80f926b01f87383dd3a1cde989c4b
Luke Gehring <lukegehring99@gmail.com>
2020-01-26 02:13:38 -0600

    Med api methods working

---
+++ apiconfig.py
@@ -0,0 +1,15 @@
+# We currently recommend using Python 3.5
+
+# Here enter your API user name
+#username = "lukegehring99@gmail.com"
+# Here enter your API password
+#password = "Yy69Tnb4N2PqEf75J"
+#priaid_authservice_url = "https://sandbox-authservice.priaid.ch/login" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://authservice.priaid.ch/login
+#priaid_healthservice_url = "https://sandbox-healthservice.priaid.ch" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://healthservice.priaid.ch
+
+username = "b9F3H_GMAIL_COM_AUT"
+password = "s5R6ZdPe48Lno9GBy"
+priaid_authservice_url = "https://authservice.priaid.ch"
+priaid_healthservice_url = "https://healthservice.priaid.ch"
+language = "en-gb" # en-gb, de-ch, fr-fr, it-it, es-es, ar-sa, ru-ru, tr-tr, sr-sp, sk-sk...
+pritnRawOutput = False # This flag can be set to see printed json data structure of webservice responses
