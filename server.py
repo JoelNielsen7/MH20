7cf6b6c60e595aab51b792eb69eacf04dc2885b2
Joel Nielsen <niels641@umn.edu>
2020-01-25 19:37:54 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask("SickoMode")
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return "Whats Up"
+
+
+
+if __name__ == "__main__":
+    # Run app
+    app.run(host="0.0.0.0", port=443)
