61c0f2f5f9be1fee3098f4154ce72f3519fbdfcd
sciguy16@gmail.com <7Jak6Jil>
2020-01-25 23:53:26 -0600

    made get db a function so it aint gross

--- db.py
+++ db.py
@@ -1,34 +1,36 @@
 import pymongo
 import pandas as pd
-myclient = pymongo.MongoClient("mongodb+srv://SickoMode:SickoMode@cluster0-zfxxk.mongodb.net/test?retryWrites=true&w=majority")
-# db = myclient.test
-# print(db)
-# dblist = myclient.list_database_names()
-# print(dblist)
-mydb = myclient["SickoMode"]
-mycol = mydb["Diseases"]
-#
-#
-#mydict = { "gender": "M", "latitude": "44052343N","longitude" "diseases": [{'name': 'buttholeaids', 'percent': 80}, {'name': 'buttholeaids2', 'percent': 90}] }
-#
-#x = mycol.insert_one(mydict)
+
+def get_db():
+    myclient = pymongo.MongoClient("mongodb+srv://SickoMode:SickoMode@cluster0-zfxxk.mongodb.net/test?retryWrites=true&w=majority")
+    # db = myclient.test
+    # print(db)
+    # dblist = myclient.list_database_names()
+    # print(dblist)
+    mydb = myclient["SickoMode"]
+    mycol = mydb["Diseases"]
+    #
+    #
+    #mydict = { "gender": "M", "latitude": "44052343N","longitude" "diseases": [{'name': 'buttholeaids', 'percent': 80}, {'name': 'buttholeaids2', 'percent': 90}] }
+    #
+    #x = mycol.insert_one(mydict)
 
 
-x = mycol.find()
+    x = mycol.find()
 
-keys = x[1].keys()
+    keys = x[1].keys()
 
-dattylist = []
-keys = ["Latitude","Longitude", "Confirmed", "Diseases" ]
-for row in x:
-    rowadd = [row["Latitude"], row["Longitude"], row["Confirmed"], row["Diseases"][0]["ID"], row["Diseases"][0]["Prob"], row["Diseases"][1]["ID"], row["Diseases"][1]["Prob"]]
-    dattylist.append(rowadd)
-
-
+    dattylist = []
+    keys = ["Latitude","Longitude", "Confirmed", "Diseases" ]
+    for row in x:
+        rowadd = [row["Latitude"], row["Longitude"], row["Confirmed"], row["Diseases"][0]["ID"], row["Diseases"][0]["Prob"], row["Diseases"][1]["ID"], row["Diseases"][1]["Prob"]]
+        dattylist.append(rowadd)
 
 
 
-df = pd.DataFrame(dattylist, columns =['latitude', 'longitude', 'confirmed', 'd1_id', 'd1_prob', 'd2_id', 'd2_prob'], dtype = float)
 
 
-df
+    df = pd.DataFrame(dattylist, columns =['latitude', 'longitude', 'confirmed', 'd1_id', 'd1_prob', 'd2_id', 'd2_prob'], dtype = float)
+
+    return(df)
+
--- map testing.py
+++ map testing.py
@@ -11,43 +11,10 @@ import os
 import branca.colormap as cm
 import re
 import numpy as np
+from db import get_db
 
 
-import pymongo
-import pandas as pd
-myclient = pymongo.MongoClient("mongodb+srv://SickoMode:SickoMode@cluster0-zfxxk.mongodb.net/test?retryWrites=true&w=majority")
-# db = myclient.test
-# print(db)
-# dblist = myclient.list_database_names()
-# print(dblist)
-mydb = myclient["SickoMode"]
-mycol = mydb["Diseases"]
-#
-#
-#mydict = { "gender": "M", "latitude": "44052343N","longitude" "diseases": [{'name': 'buttholeaids', 'percent': 80}, {'name': 'buttholeaids2', 'percent': 90}] }
-#
-#x = mycol.insert_one(mydict)
-
-
-x = mycol.find()
-
-keys = x[1].keys()
-
-dattylist = []
-keys = ["Latitude","Longitude", "Confirmed", "Diseases" ]
-for row in x:
-    rowadd = [row["Latitude"], row["Longitude"], row["Confirmed"], row["Diseases"][0]["ID"], row["Diseases"][0]["Prob"], row["Diseases"][1]["ID"], row["Diseases"][1]["Prob"]]
-    dattylist.append(rowadd)
-
-
-
-
-
-df = pd.DataFrame(dattylist, columns =['latitude', 'longitude', 'confirmed', 'd1_id', 'd1_prob', 'd2_id', 'd2_prob'], dtype = float)
-
-
-data = df
-
+data = get_db()
 
 
 this_map = folium.Map(prefer_canvas=True)
